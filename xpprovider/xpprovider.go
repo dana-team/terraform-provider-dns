41f66074dc771beaf8c30af9e26c180d170b7607
mzeevi <meytar80@gmail.com>
2024-04-28 09:39:09 +0000

    added more functions to xpprovider
    Signed-off-by: mzeevi <meytar80@gmail.com>

--- xpprovider/xpprovider.go
+++ xpprovider/xpprovider.go
@@ -2,10 +2,34 @@ package xpprovider
 
 import (
 	"context"
+	fwprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+	fwschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-dns/internal/provider"
 )
 
-func GetProvider(_ context.Context) (*schema.Provider, error) {
-	return provider.New(), nil
+// GetProvider returns new provider instances for both Terraform Plugin Framework provider of type provider.Provider
+// and Terraform Plugin SDKv2 provider of type *schema.Provider
+// provider
+func GetProvider(_ context.Context) (fwprovider.Provider, *schema.Provider) {
+	p := provider.New()
+	fwProvider := provider.NewFrameworkProvider()
+
+	return fwProvider, p
+}
+
+// GetProviderSchema returns the Terraform Plugin SDKv2 provider schema of the provider
+func GetProviderSchema(_ context.Context) *schema.Provider {
+	return provider.New()
+}
+
+// GetFrameworkProviderSchema returns the Terraform Plugin Framework provider schema of the provider
+func GetFrameworkProviderSchema(ctx context.Context) (fwschema.Schema, error) {
+	fwProvider, _ := GetProvider(ctx)
+
+	schemaReq := fwprovider.SchemaRequest{}
+	schemaResp := fwprovider.SchemaResponse{}
+	fwProvider.Schema(ctx, schemaReq, &schemaResp)
+
+	return schemaResp.Schema, nil
 }
